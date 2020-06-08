a892cab526ccbc1be1799e870c811fe403239697
hassaan <hassaanalansary@yahoo.com>
2020-06-08 13:28:09 +0200

    Send approperiate signal when querying item_inventory

--- django_quickbooks/processors/pos/item_inventory.py
+++ django_quickbooks/processors/pos/item_inventory.py
@@ -1,7 +1,7 @@
 from django_quickbooks import QUICKBOOKS_ENUMS, qbwc_settings
 from django_quickbooks.objects.item_inventory_pos import ItemInventory
 from django_quickbooks.processors.base import ResponseProcessor, ResponseProcessorMixin
-from django_quickbooks.signals import qb_object_created
+from django_quickbooks.signals import qb_object_created, qb_object_queried
 from django.utils import timezone
 
 LocalItemInventory = qbwc_settings.LOCAL_MODEL_CLASSES['ItemInventory']
@@ -28,7 +28,7 @@ class ItemInventoryQueryResponseProcessor(ResponseProcessor, ResponseProcessorMi
                     local_item.qbd_object_updated_at = timezone.now()
                     local_item.save()
                     # self.update(local_item, item)
-                    qb_object_created.send(self.obj_class, instance=local_item, realm=realm)
+                    qb_object_queried.send(self.obj_class, instance=local_item, realm=realm)
             # else:
             #     self.create(item_inventory)
         return True
