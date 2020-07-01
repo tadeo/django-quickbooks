6cce9d792552b31dba62a446e4ee7166dd34e145
hassaan <hassaanalansary@yahoo.com>
2020-07-01 14:13:48 +0200

    Change to voucher processors to the new signals method

--- django_quickbooks/processors/pos/voucher.py
+++ django_quickbooks/processors/pos/voucher.py
@@ -1,8 +1,7 @@
 from django_quickbooks import QUICKBOOKS_ENUMS, qbwc_settings
 from django_quickbooks.objects.voucher_pos import Voucher
 from django_quickbooks.processors.base import ResponseProcessor, ResponseProcessorMixin
-from django_quickbooks.signals import qb_object_created
-from django.utils import timezone
+from django_quickbooks.signals import qb_object_created, qb_object_queried
 
 LocalVoucher = qbwc_settings.LOCAL_MODEL_CLASSES['VoucherPOS']
 
@@ -14,22 +13,13 @@ class VoucherQueryResponseProcessor(ResponseProcessor, ResponseProcessorMixin):
     obj_class = Voucher
 
     def process(self, realm):
-        # fixme should handle duplicate response in someway
         cont = super().process(realm)
         if not cont:
             return False
         for voucher_ret in list(self._response_body):
             voucher = self.obj_class.from_lxml(voucher_ret)
-            local_voucher = None
-            if voucher.ListID:
-                local_voucher = self.find_by_list_id(voucher.ListID)
-            elif not local_voucher and voucher.Name:
-                local_voucher = self.find_by_name(voucher.Name)
+            qb_object_queried.send(self.obj_class, instance=voucher, realm=realm)
 
-            if local_voucher:
-                self.update(local_voucher, voucher)
-            # else:
-            #     self.create(voucher)
         return True
 
 
@@ -40,19 +30,11 @@ class VoucherAddResponseProcessor(ResponseProcessor, ResponseProcessorMixin):
     obj_class = Voucher
 
     def process(self, realm):
-        # fixme should handle duplicate error response in someway
         cont = super().process(realm)
         if not cont:
             return False
         for voucher_ret in list(self._response_body):
             voucher = self.obj_class.from_lxml(voucher_ret)
-            if voucher.InvoiceNumber:
-                local_voucher = self.find_by_name(voucher.InvoiceNumber, field_name='id')
+            qb_object_created.send(self.obj_class, instance=voucher, realm=realm)
 
-                if local_voucher:
-                    local_voucher.qbd_object_id = voucher.ListID
-                    local_voucher.qbd_object_updated_at = timezone.now()
-                    local_voucher.save()
-                    # self.update(local_voucher, voucher)
-                    qb_object_created.send(local_voucher.__class__, instance=local_voucher, realm=realm)
         return True
