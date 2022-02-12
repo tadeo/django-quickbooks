9c91a7c512da182353978cbcbd0f9517b5143a11
Henry Bradlow <henrybrad@gmail.com>
2022-02-12 13:29:24 -0800

    Fixes for Django3.

--- django_quickbooks/signals/__init__.py
+++ django_quickbooks/signals/__init__.py
@@ -1,24 +1,17 @@
 from django.dispatch import Signal
 
-qbd_task_create = Signal(providing_args=[
-    "qb_operation",
-    "qb_resource",
-    "object_id",
-    "content_type",
-    "realm_id",
-    "instance",
-])
+qbd_task_create = Signal()
 
-customer_created = Signal(providing_args=["qbd_model_mixin_obj", "realm_id"])
-customer_updated = Signal(providing_args=["qbd_model_mixin_obj", "realm_id"])
-invoice_created = Signal(providing_args=["qbd_model_mixin_obj", "realm_id"])
-invoice_updated = Signal(providing_args=["qbd_model_mixin_obj", "realm_id"])
+customer_created = Signal()
+customer_updated = Signal()
+invoice_created = Signal()
+invoice_updated = Signal()
 
-qb_object_created = Signal(providing_args=["instance", "realm"])
-qb_object_updated = Signal(providing_args=["instance", "realm"])
-qb_object_queried = Signal(providing_args=["instance", "realm"])
-realm_authenticated = Signal(providing_args=["realm"])
-qbd_first_time_connected = Signal(providing_args=["realm_id"])
+qb_object_created = Signal()
+qb_object_updated = Signal()
+qb_object_queried = Signal()
+realm_authenticated = Signal()
+qbd_first_time_connected = Signal()
 
 from django_quickbooks.signals.customer import *
 from django_quickbooks.signals.invoice import *
